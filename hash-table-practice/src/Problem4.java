6237df7dbb7900e4b2068dcfef589714c1e1ed83
Geethika-92 <geethikatalari927@gmail.com>
2026-03-13 09:07:14 +0530

    Added problems 1-5

--- hash-table-practice/src/Problem4.java
+++ hash-table-practice/src/Problem4.java
@@ -1,2 +1,38 @@
+import java.util.*;
+
 public class Problem4 {
+
+    static HashMap<String, Set<String>> ngramMap = new HashMap<>();
+
+    public static void addDocument(String docId, String text) {
+        String[] words = text.split(" ");
+
+        for (int i = 0; i < words.length - 2; i++) {
+            String gram = words[i] + " " + words[i+1] + " " + words[i+2];
+
+            ngramMap.putIfAbsent(gram, new HashSet<>());
+            ngramMap.get(gram).add(docId);
+        }
+    }
+
+    public static void checkSimilarity(String text) {
+        String[] words = text.split(" ");
+        int matches = 0;
+
+        for (int i = 0; i < words.length - 2; i++) {
+            String gram = words[i] + " " + words[i+1] + " " + words[i+2];
+
+            if (ngramMap.containsKey(gram)) {
+                matches++;
+            }
+        }
+
+        System.out.println("Matching n-grams: " + matches);
+    }
+
+    public static void main(String[] args) {
+        addDocument("doc1", "this is a simple plagiarism detection test");
+
+        checkSimilarity("this is a simple plagiarism test");
+    }
 }
--- hash-table-practice/src/Problem5.java
+++ hash-table-practice/src/Problem5.java
@@ -1,2 +1,32 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Problem5 {
+
+    static HashMap<String, Integer> pageViews = new HashMap<>();
+    static HashMap<String, HashSet<String>> uniqueVisitors = new HashMap<>();
+
+    public static void processEvent(String url, String userId) {
+
+        pageViews.put(url, pageViews.getOrDefault(url, 0) + 1);
+
+        uniqueVisitors.putIfAbsent(url, new HashSet<>());
+        uniqueVisitors.get(url).add(userId);
+    }
+
+    public static void showStats() {
+        for (String url : pageViews.keySet()) {
+            System.out.println(url + " Views: " + pageViews.get(url) +
+                    " Unique Visitors: " + uniqueVisitors.get(url).size());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        processEvent("/news", "user1");
+        processEvent("/news", "user2");
+        processEvent("/sports", "user1");
+
+        showStats();
+    }
 }
