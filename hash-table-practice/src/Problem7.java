b368f37fae174a4e8df303c52e5510fafa36af97
Geethika-92 <geethikatalari927@gmail.com>
2026-03-13 09:11:43 +0530

    Added problems 6-10

--- hash-table-practice/src/Problem7.java
+++ hash-table-practice/src/Problem7.java
@@ -1,2 +1,26 @@
+import java.util.*;
+
 public class Problem7 {
+
+    static HashMap<String, Integer> searchMap = new HashMap<>();
+
+    public static void addSearch(String query) {
+        searchMap.put(query, searchMap.getOrDefault(query, 0) + 1);
+    }
+
+    public static void suggest(String prefix) {
+        for (String key : searchMap.keySet()) {
+            if (key.startsWith(prefix)) {
+                System.out.println(key + " (" + searchMap.get(key) + ")");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        addSearch("java tutorial");
+        addSearch("java download");
+        addSearch("javascript basics");
+
+        suggest("jav");
+    }
 }
